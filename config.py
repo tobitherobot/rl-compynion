3186f28685dba129c455cd942b2407d06ca8d1f1
Marlon Gundelfinger <marlonqgundelfinger@gmail.com>
2020-02-16 22:29:52 +0100

    removed src directory

--- config.py
+++ config.py
@@ -2,15 +2,15 @@ import os
 
 
 # MySQL Connection
-DATABASE_URL = '127.0.0.1'
+DATABASE_URL = '192.168.178.222'
 USERNAME = 'root'
-PASSWORD = 'admin'
+PASSWORD = 'external'
 DATABASE = 'rl_compynion'
 
 # File Storage Directory
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) + '\\'
 
 
-RESOURCES_DIR = os.path.join(ROOT_DIR + 'src\\resources\\')
+RESOURCES_DIR = os.path.join(ROOT_DIR + 'resources\\')
 REPLAYS_DIR = os.path.join(RESOURCES_DIR + 'replays\\')
 JSON_DIR = os.path.join(RESOURCES_DIR + 'json\\')
